29ee8cb11aad3000d6847507ee9e4e7dfe3789fe
Alex Kim <312akim@gmail.com>
2022-02-04 18:31:11 -0800

    Setup: Initialize Library

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+// Commonjs modules (require syntax) can import json into javascript variable
+const packageJson = require("./package.json");
+
+// Array of what you want to bundle and export
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  
+  // Handle exporting types for TypeScript users
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
