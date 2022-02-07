29133da7936b7f2bc22e01d82bcb8f711d63350d
Alex Kim <312akim@gmail.com>
2022-02-07 10:33:27 -0800

    Optimization: Added terser minifier and moved react to peerDep

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from 'rollup-plugin-postcss';
 
@@ -26,10 +27,13 @@ export default [
     ],
     plugins: [
       peerDepsExternal(),
+      
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      postcss()
+      postcss(),
+
+      terser(),
     ],
     external: ["styled-components"]
   },
